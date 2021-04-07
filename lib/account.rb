3c86fd07b3bd5f930d177e3665a579de5c1a9bae
Lee Jones <lee@lees-mbp.lan>
2021-04-07 18:09:59 +0100

    fixed rubocop complaints

--- lib/account.rb
+++ lib/account.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require_relative 'statement'
 
 class Account
@@ -9,26 +11,27 @@ class Account
     @time = Time.new
     @statement = statement
   end
-  
+
   def withdraw(amount)
     @balance -= amount
-    new_transaction = ("#{@time.strftime('%d/%m/%y')}  || #{'%.2f' % amount}    || nil      || #{'%.2f' % @balance}")
+    new_transaction = "#{@time.strftime('%d/%m/%y')}  || #{'%.2f' % amount}    || nil      || #{format('%.2f',
+                                                                                                       @balance)}"
     @transaction.push(new_transaction)
   end
 
   def deposit(amount)
     @balance += amount
-    new_transaction = ("#{@time.strftime('%d/%m/%y')}  || nil      || #{'%.2f' % amount}   || #{'%.2f' % @balance}")
+    new_transaction = "#{@time.strftime('%d/%m/%y')}  || nil      || #{'%.2f' % amount}   || #{format('%.2f',
+                                                                                                      @balance)}"
     @transaction.push(new_transaction)
   end
-  
+
   def bank_statement
     @statement.print_statement(@transaction)
   end
 
-# Testing rspec is working
+  # Testing rspec is working
   def this_is_test
-    "This is a test"
+    'This is a test'
   end
-
 end
--- lib/statement.rb
+++ lib/statement.rb
@@ -1,23 +1,21 @@
-class Statement
+# frozen_string_literal: true
 
+class Statement
   HEADER = "date      || withdraw || deposit  || balance  \n"
-  
+
   def print_statement(transaction)
     new_statement = transaction.reverse.join("\n")
     this_statement = HEADER + new_statement
     puts this_statement
   end
 
+  #   date     || deposit  || withdraw  || balance
+  # 14/01/2012 ||          || 500.00    || 2500.00
+  # 13/01/2012 || 2000.00  ||           || 3000.00
+  # 10/01/2012 || 1000.00  ||           || 1000.00
 
- 
-#   date     || deposit  || withdraw  || balance
-# 14/01/2012 ||          || 500.00    || 2500.00
-# 13/01/2012 || 2000.00  ||           || 3000.00
-# 10/01/2012 || 1000.00  ||           || 1000.00
-
-# Testing rspec is working
+  # Testing rspec is working
   def this_is_test
-    "This is a test"
+    'This is a test'
   end
-
 end
